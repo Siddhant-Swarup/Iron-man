7a82c23e89e652065cedf3e6e0fb7142e2fdd6a5
Siddhant-Swarup <swarupsiddhant311@gmail.com>
2021-06-21 22:08:17 +0530

    Second Commit

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function preload() {
   soundFormats('mp3');
   explosion = loadSound("sounds/Explosion");
   diamonds_sound = loadSound("sounds/Diamonds");
-  electro_effect=loadSound("sounds/Electro effect 2");
+  electro_effect = loadSound("sounds/Electro effect 2");
 }
 
 
@@ -33,6 +33,7 @@ function setup() {
 
   edge1 = createSprite(0, 5, 2000, 10);//top
   edge2 = createSprite(0, 565, 2000, 10);//bottom
+  edge2.visible = false;
   edge3 = createSprite(5, 0, 10, 2000);//left
   edge4 = createSprite(995, 0, 10, 2000);//right
 
@@ -43,54 +44,70 @@ function setup() {
 }
 
 function draw() {
+  if (gamemode === "play") {
+    bg.addImage(backgroundImg);
+    bg.scale = 2;
+    if (bg.y < 100) {
+      bg.y = bg.height / 4;
+    }
 
-  bg.addImage(backgroundImg);
-  bg.scale = 2;
-  if (bg.y < 100) {
-    bg.y = bg.height / 4;
-  }
 
 
+    //for the edges
+    iron_man.collide(edge1);
+    iron_man.collide(edge2);
+    iron_man.collide(edge3);
+    iron_man.collide(edge4);
 
-  //for the edges
-  iron_man.collide(edge1);
-  iron_man.collide(edge2);
-  iron_man.collide(edge3);
-  iron_man.collide(edge4);
+    //IRON MAN INTERACTION WITH STONES
+    for (let i = 0; i < StoneGroup.length; i++) {
+      let temp = StoneGroup.get(i);
+      if (temp.isTouching(iron_man)) {
+        iron_man.collide(temp);
+      }
+    }
 
-  //IRON MAN INTERACTION WITH STONES
-  for (let i = 0; i < StoneGroup.length; i++) {
-    let temp = StoneGroup.get(i);
-    if (temp.isTouching(iron_man)) {
-      iron_man.collide(temp);
+    //IRON MAN INTERACTION WITH DIAMONDS
+    for (let i = 0; i < DiamondGroup.length; i++) {
+      let temp = DiamondGroup.get(i);
+
+      if (temp.isTouching(iron_man)) {
+        diamonds_sound.play();
+        temp.destroy();
+        temp = null;
+        score++;
+      }
+    }
+
+    //IRON MAN INTERACTION WITH SPIKES
+    for (let i = 0; i < SpikeGroup.length; i++) {
+      let temp = SpikeGroup.get(i);
+
+      if (temp.isTouching(iron_man)) {
+        // electro_effect.play();
+        temp.destroy();
+        temp = null;
+        score -= 2;
+      }
+    }
+    if (edge2.isTouching(iron_man)) {
+      iron_man.changeImage("collide", iron_man_collide);
+      gamemode = "end";
+      explosion.play();
+
     }
   }
 
-  //IRON MAN INTERACTION WITH DIAMONDS
-  for (let i = 0; i < DiamondGroup.length; i++) {
-    let temp = DiamondGroup.get(i);
+  if (gamemode === "end") {
+    bg.y = 0;
+    DiamondGroup.setVelocityYEach(0);
+    StoneGroup.setVelocityYEach(0);
+    SpikeGroup.setVelocityYEach(0);
+    SpikeGroup.setLifetimeEach(-1);
+    DiamondGroup.setLifetimeEach(-1);
+    StoneGroup.setLifetimeEach(-1);
 
-    if (temp.isTouching(iron_man)) {
-      score++;
-      temp.destroy();
-      diamonds_sound.play();
-      temp = null;
-    }
-  }
 
-  //IRON MAN INTERACTION WITH SPIKES
-  for (let i = 0; i < SpikeGroup.length; i++) {
-    let temp = SpikeGroup.get(i);
-    if (temp.isTouching(iron_man)) {
-      score -= 2;
-      temp.destroy();
-      electro_effect.play();
-      temp = null;
-    }
-  }
-  if (edge2.isTouching(iron_man)) {
-    iron_man.changeImage("collide", iron_man_collide);
-    explosion.play();
 
   }
 
@@ -125,7 +142,7 @@ function controls() {
 //THIS FUNCTION WILL GENERATE STONE
 function generatestone() {
   if (frameCount % 30 === 0) {
-    stone = createSprite(1100, 0, 10, 8);
+    stone = createSprite(1100, -670, 10, 8);
     stone.x = random(50, 900);
     stone.addImage(stoneimage);
     stone.scale = 0.5;
@@ -138,7 +155,7 @@ function generatestone() {
 //THIS FUNCTION WILL GENEARATE DIAMONDS
 function generatediamonds() {
   if (frameCount % 100 === 0) {
-    diamonds = createSprite(1100, 0, 5, 5);
+    diamonds = createSprite(1000, -670, 5, 5);
     diamonds.x = random(50, 900);
     diamonds.addImage(dimaondImage);
     diamonds.scale = 0.5;
@@ -151,7 +168,7 @@ function generatediamonds() {
 //THIS FUNCTION WILL GENERATE SPIKES
 function generateSpikes() {
   if (frameCount % 100 === 0) {
-    spike = createSprite(1100, 0, 5, 5);
+    spike = createSprite(1100, -670, 5, 5);
     spike.x = random(50, 900);
     spike.addImage(spikeImage);
     spike.scale = 0.5;
