097544ec48c76eb84acad83a120d682ba788c3bd
Siddhant-Swarup <swarupsiddhant311@gmail.com>
2021-06-21 22:11:53 +0530

    third commit

---
+++ Gamemode.js
@@ -0,0 +1,14 @@
+class Status{
+    constructor(){
+        this.value="";
+    }
+    setvalue(val){
+        this.value=val;
+    }
+    getvalue(){
+        return this.value;
+    }
+    displayvalue(){
+        console.log(this.value);
+    }
+}
